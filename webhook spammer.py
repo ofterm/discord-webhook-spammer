46105cab9e2814d9530d8f5f042c538a212a5ade
ofterm <134554182+ofterm@users.noreply.github.com>
2023-05-24 21:49:00 +0100

    Add files via upload

---
+++ webhook spammer.py
@@ -0,0 +1,19 @@
+import time
+import requests
+
+webhook = "your webhook here"
+message = "your message here"
+
+def send_webhook_message(url, message):
+    data = {
+        "content": message
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 204:
+        print("Message sent successfully!")
+    else:
+        print(f"rate limit, this shouldn't happen more than 5 times in a row")
+
+while True:
+    send_webhook_message(webhook, message)
+    time.sleep(0.05)  
